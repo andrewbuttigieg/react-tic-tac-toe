f87fb07862e90d2cfd44fc04304f8b75b04f8461
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2019-12-12 10:43:10 +0100

    working on game x/o rendering

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Game from './Game';
+import Shoppinglist from './Shoppinglist'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Game />, document.getElementById('root'));
+// ReactDOM.render(<Shoppinglist />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
