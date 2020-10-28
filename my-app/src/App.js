a2c03d11748013188fe30243341777fd7e412098
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2020-10-28 08:07:46 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,21 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class ShoppingList extends React.Component {
-  render() {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
-    );
-  }
-}
-
 function App() {
   return (
     <div className="App">
